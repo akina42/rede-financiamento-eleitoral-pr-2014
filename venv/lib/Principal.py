5679fa4d09067aaea86404693b7f5a64f611eec2
akina42 <akina_ak@hotmail.com>
2018-08-21 20:54:52 -0300

    Add/Change - Campo para rótulo e alteração na geração do arquivo para autovetor

--- venv/lib/Principal.py
+++ venv/lib/Principal.py
@@ -3,6 +3,7 @@ import networkx as nx
 
 #Criar um dicionário para cada atributo
 
+nome_rotulo_vertice = {}
 cnpj_prestador = {}
 tipo_sequencial = {}
 sequencial = {}
@@ -14,6 +15,8 @@ nome_candidato = {}
 cpf_candidato = {}
 cpf_cnpj_doador = {}
 nome_doador_receita = {}
+
+#TODO mapear nome do rotulo
 
 ###############################################
 
@@ -33,6 +36,8 @@ for registro_partidos in registros_partidos:
         sequencial[nos_partidos[-1]] = registro_partidos[4]
         uf[nos_partidos[-1]] = registro_partidos[5]
         sigla_partido[nos_partidos[-1]] = registro_partidos[7]
+        nome_rotulo_vertice[-1] = sigla_partido[nos_partidos[-1]] + " " + sequencial[nos_partidos[-1]]
+
 
     if (registro_partidos[10] not in nos_partidos and registro_partidos[10] != "#NULO"):
         nos_partidos.append(registro_partidos[10])
@@ -85,6 +90,8 @@ for registro_comites in registros_comites:
         sequencial[nos_comites[-1]] = registro_comites[4]
         uf[nos_comites[-1]] = registro_comites[5]
         sigla_partido[nos_comites[-1]] = registro_comites[7]
+        nome_rotulo_vertice[-1] = sigla_partido[nos_comites[-1]] + " " + sequencial[nos_comites[-1]]
+
 
     if (registro_comites[10] not in nos_comites and registro_comites[10] != "#NULO"):
         nos_comites.append(registro_comites[10])
@@ -143,6 +150,7 @@ for registro_candidatos in registros_candidatos:
         cargo_candidato[nos_candidatos[-1]] = registro_candidatos[8]
         nome_candidato[nos_candidatos[-1]] = registro_candidatos[9]
         cpf_candidato[nos_candidatos[-1]] = registro_candidatos[10]
+        nome_rotulo_vertice[-1] = sigla_partido[nos_candidatos[-1]]  + " " + nome_candidato[nos_candidatos[-1]]
 
     if (registro_candidatos[13] not in nos_candidatos and registro_candidatos[13] != "#NULO"):
         nos_candidatos.append(registro_candidatos[13])
@@ -227,7 +235,15 @@ print("1 - Cálculo da centralidade de autovetor")
 #eigenvector_centrality_numpy(G, weight=None, max_iter=50, tol=0)
 
 eigenvector_centrality = nx.eigenvector_centrality_numpy(DG, "weight", len(DG), 0)
-print(['{} {:0.20f}'.format(node, eigenvector_centrality[node]) for node in eigenvector_centrality], file=open('/home/akina/Dropbox/TCC/Dados/eigenvector_centrality.txt', "w"))
+#print(['{}, {:0.20f}'.format(node, eigenvector_centrality[node]) for node in eigenvector_centrality], file=open('/home/akina/Dropbox/TCC/Dados/teste_eigenvector_centrality.csv', "w"))
+
+dict_nome_rotulo_vertice = nx.get_node_attributes(DG, 'nome_rotulo_vertice')
+
+csv_file_eigenvector = '/home/akina/Dropbox/TCC/Dados/teste_eigenvector_centrality.csv'
+with open(csv_file_eigenvector, "w") as output_eigenvector:
+    writer = csv.writer(output_eigenvector)
+    for node in sorted(eigenvector_centrality, key=lambda x: eigenvector_centrality[x], reverse=True):
+        writer.writerow(["{}".format(DG.node[node]), "{:0.20f}".format(eigenvector_centrality[node])])
 
 
 ################################################
@@ -235,7 +251,7 @@ print("2 - Cálculo da centralidade de grau")
 #degree_centrality(G)
 
 degree_centrality = nx.degree_centrality(DG)
-print(['{} {:0.20f}'.format(node, degree_centrality[node]) for node in degree_centrality], file=open('/home/akina/Dropbox/TCC/Dados/degree_centrality.txt', "w"))
+print(['{}, {:0.20f} \n'.format(node, degree_centrality[node]) for node in degree_centrality], file=open('/home/akina/Dropbox/TCC/Dados/teste_degree_centrality.csv', "w"))
 
 
 ################################################
@@ -243,7 +259,7 @@ print("3 - Cálculo da centralidade de intermediação")
 #betweenness_centrality(G, k=None, normalized=True, weight=None, endpoints=False, seed=None)
 
 betweenness_centrality = nx.betweenness_centrality(DG, len(DG), True, "weight", False, None)
-print(['{} {:0.20f}'.format(node, betweenness_centrality[node]) for node in betweenness_centrality], file=open('/home/akina/Dropbox/TCC/Dados/betweenness_centrality.txt', "w"))
+print(['{}, {:0.20f} \n'.format(node, betweenness_centrality[node]) for node in betweenness_centrality], file=open('/home/akina/Dropbox/TCC/Dados/teste_betweenness_centrality.csv', "w"))
 
 
 ################################################
@@ -251,4 +267,6 @@ print("4 - Cálculo da centralidade de proximidade")
 #closeness_centrality(G, u=None, distance=None, wf_improved=True, reverse=False)
 
 closeness_centrality = nx.closeness_centrality(DG, None, None, True, True)
-print(['{} {:0.20f}'.format(node, closeness_centrality[node]) for node in closeness_centrality], file=open('/home/akina/Dropbox/TCC/Dados/closeness_centrality.txt', "w"))
+print(['{}, {:0.20f} \n'.format(node, closeness_centrality[node]) for node in closeness_centrality], file=open('/home/akina/Dropbox/TCC/Dados/teste_closeness_centrality.csve', "w"))
+
+
